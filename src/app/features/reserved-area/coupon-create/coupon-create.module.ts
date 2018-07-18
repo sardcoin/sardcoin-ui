dd34e329575ff22447dd5c5051c75db7427854a0
Raffaele <raffaele.porcu@flosslab.com>
2018-07-18 14:29:51 +0200

    init configure page create coupon and list coupon

--- src/app/features/reserved-area/coupon-create/coupon-create.module.ts
+++ src/app/features/reserved-area/coupon-create/coupon-create.module.ts
@@ -1,11 +1,12 @@
 import {NgModule} from "@angular/core";
 import {FeatureReservedAreaCouponCreateComponent} from "./coupon-create.component";
-
+import {ReactiveFormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
     FeatureReservedAreaCouponCreateComponent
   ],
   imports: [
+    ReactiveFormsModule
   ],
   exports: [
     FeatureReservedAreaCouponCreateComponent
---
+++ src/app/features/reserved-area/coupon-item/coupon-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-coupon-item',
+  templateUrl: './coupon-item.component.html',
+  styleUrls: ['./coupon-item.component.scss']
+})
+export class CouponItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
