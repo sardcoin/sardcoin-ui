5702acca59b086f1e5846ed5300e8a65581514ff
Raffaele <raffaele.porcu@flosslab.com>
2018-12-06 17:16:28 +0100

    import paypal done

--- src/app/features/reserved-area/consumer/payment-confirm/payment-confirm.component.ts
+++ src/app/features/reserved-area/consumer/payment-confirm/payment-confirm.component.ts
@@ -10,6 +10,8 @@ import {UserService} from '../../../../shared/_services/user.service';
 import {BsModalRef} from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import {StoreService} from '../../../../shared/_services/store.service';
 import {Breadcrumb} from '../../../../core/breadcrumb/Breadcrumb';
+import { of } from 'rxjs';
+import { PayPalConfig, PayPalEnvironment, PayPalIntegrationType } from 'ngx-paypal';
 
 @Component({
   selector: 'app-payment-detail',
@@ -18,6 +20,7 @@ import {Breadcrumb} from '../../../../core/breadcrumb/Breadcrumb';
 })
 export class PaymentConfirmComponent implements OnInit, OnDestroy {
 
+  public payPalConfig?: PayPalConfig;
 
   cartArray: any;
   user: any;
@@ -48,6 +51,7 @@ export class PaymentConfirmComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
+    this.initConfig();
     this.addBreadcrumb();
     this.localStorage.getItem('cart').subscribe(cart => {
       this.cartArray = cart;
@@ -130,6 +134,97 @@ export class PaymentConfirmComponent implements OnInit, OnDestroy {
   toastBuy() {
     this.toastr.success( 'Your purchase has successfully completed', 'Congratulations!');
 
+  }
+
+
+  private initConfig(): void {
+    this.payPalConfig = new PayPalConfig(
+      PayPalIntegrationType.ClientSideREST,
+      PayPalEnvironment.Sandbox,
+      {
+        commit: true,
+        client: {
+          sandbox:
+            'AZDxjDScFpQtjWTOUtWKbyN_bDt4OgqaF4eYXlewfBP4-8aqX3PiV8e1GWU6liB2CUXlkA59kJXE7M6R'
+        },
+        button: {
+          label: 'paypal',
+          layout: 'vertical'
+        },
+        onAuthorize: (data, actions) => {
+          console.log('Authorize');
+          return of(undefined);
+        },
+        onPaymentComplete: (data, actions) => {
+          console.log('OnPaymentComplete');
+        },
+        onCancel: (data, actions) => {
+          console.log('OnCancel');
+        },
+        onError: err => {
+          console.log('OnError');
+        },
+        onClick: () => {
+          console.log('onClick');
+        },
+        validate: (actions) => {
+          console.log(actions);
+        },
+        experience: {
+          noShipping: true,
+          brandName: 'Angular PayPal'
+        },
+        transactions: [
+          {
+            amount: {
+              total: 30.11,
+              currency: 'USD',
+              details: {
+                subtotal: 30.00,
+                tax: 0.07,
+                shipping: 0.03,
+                handling_fee: 1.00,
+                shipping_discount: -1.00,
+                insurance: 0.01
+              }
+            },
+            custom: 'Custom value',
+            item_list: {
+              items: [
+                {
+                  name: 'hat',
+                  description: 'Brown hat.',
+                  quantity: 5,
+                  price: 3,
+                  tax: 0.01,
+                  sku: '1',
+                  currency: 'USD'
+                },
+                {
+                  name: 'handbag',
+                  description: 'Black handbag.',
+                  quantity: 1,
+                  price: 15,
+                  tax: 0.02,
+                  sku: 'product34',
+                  currency: 'USD'
+                }],
+              shipping_address: {
+                recipient_name: 'Brian Robinson',
+                line1: '4th Floor',
+                line2: 'Unit #34',
+                city: 'San Jose',
+                country_code: 'US',
+                postal_code: '95131',
+                phone: '011862212345678',
+                state: 'CA'
+              },
+            },
+          }
+        ],
+        note_to_payer: 'Contact us if you have troubles processing payment'
+      }
+    );
   }
 
 }
