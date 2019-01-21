32b5f0bc82de1c4f07b941a71a57b716e095ede8
Ibbus93 <ibbus93@gmail.com>
2019-01-21 11:51:51 +0100

    G Maps done

--- src/app/features/reserved-area/consumer/coupon-bought/coupon-bought-detail/coupon-bought-detail.component.ts
+++ src/app/features/reserved-area/consumer/coupon-bought/coupon-bought-detail/coupon-bought-detail.component.ts
@@ -1,45 +1,38 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewEncapsulation} from '@angular/core';
 import {environment} from '../../../../../../environments/environment';
-import {BsModalRef} from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import {Coupon} from '../../../../../shared/_models/Coupon';
 import {BreadcrumbActions} from '../../../../../core/breadcrumb/breadcrumb.actions';
-import {LocalStorage} from '@ngx-pwa/local-storage';
 import {CouponService} from '../../../../../shared/_services/coupon.service';
-import {BsModalService} from 'ngx-bootstrap/modal';
 import {Router} from '@angular/router';
-import {ToastrService} from 'ngx-toastr';
 import {Breadcrumb} from '../../../../../core/breadcrumb/Breadcrumb';
 import {UserService} from '../../../../../shared/_services/user.service';
 
 @Component({
   selector: 'app-coupon-bought-detail',
   templateUrl: './coupon-bought-detail.component.html',
-  styleUrls: ['./coupon-bought-detail.component.scss']
+  styleUrls: ['./coupon-bought-detail.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CouponBoughtDetailComponent implements OnInit, OnDestroy { // TODO delete (redundant)
-  URLstring = environment.protocol + '://' + environment.host + ':' + environment.port + '/';
-  modalRef: BsModalRef;
+  imageURL = environment.protocol + '://' + environment.host + ':' + environment.port + '/';
   couponPass: Coupon;
   cart = new Coupon();
   producer = null;
 
-  constructor(private breadcrumbActions: BreadcrumbActions,
-              private couponService: CouponService,
-              private router: Router,
-              private modalService: BsModalService,
-              private toastr: ToastrService,
-              private userService: UserService,
-              protected localStorage: LocalStorage) {
+  constructor(
+    private breadcrumbActions: BreadcrumbActions,
+    private couponService: CouponService,
+    private router: Router,
+    private userService: UserService,
+  ) {
   }
 
   ngOnInit() {
     this.couponService.currentMessage.subscribe(coupon => {
-      this.couponPass = coupon;
-
-      if (this.couponPass === null) {
+      if (coupon === null) {
         this.router.navigate(['/reserved-area/consumer/bought']);
       } else {
-        this.URLstring = this.URLstring + this.couponPass.image;
+        this.couponPass = coupon;
         this.addBreadcrumb();
         this.getOwner();
       }
@@ -54,8 +47,6 @@ export class CouponBoughtDetailComponent implements OnInit, OnDestroy { // TODO 
     const bread = [] as Breadcrumb[];
 
     bread.push(new Breadcrumb('Home', '/reserved-area/consumer/'));
-    // bread.push(new Breadcrumb('Reserved Area', '/reserved-area/'));
-    // bread.push(new Breadcrumb('Consumer', '/reserved-area/consumer/'));
     bread.push(new Breadcrumb('My Purchases', '/reserved-area/consumer/bought/'));
     bread.push(new Breadcrumb(this.couponPass.title + ' details', '/reserved-area/consumer/bought/details'));
 
@@ -67,31 +58,21 @@ export class CouponBoughtDetailComponent implements OnInit, OnDestroy { // TODO 
   }
 
   formatPrice(price) {
-    if (price === 0) {
-      return 'Free';
-    }
-    return '€ ' + price.toFixed(2);
+    return price === 0 ? 'Free' : '€ ' + price.toFixed(2);
   }
 
   formatUntil(until) {
-    if (until === null) {
-      return 'Unlimited';
-    }
-
-    return this.formatFrom(until);
+    return until ? this.formatDate(until) : 'Unlimited';
   }
 
 
-  formatFrom(dataFrom) {
-    const date = dataFrom.toString().substring(0, dataFrom.indexOf('T'));
-    const time = dataFrom.toString().substring(dataFrom.indexOf('T') + 1, dataFrom.indexOf('Z'));
+  formatDate(inptuDate) {
+    const date = inptuDate.toString().substring(0, inptuDate.indexOf('T'));
+    const time = inptuDate.toString().substring(inptuDate.indexOf('T') + 1, inptuDate.indexOf('Z'));
     return 'Date: ' + date + ' Time: ' + time;
   }
 
-
   retry() {
-    this.removeBreadcrumb();
-
     this.router.navigate(['/reserved-area/consumer/bought']);
   }
 
@@ -99,9 +80,7 @@ export class CouponBoughtDetailComponent implements OnInit, OnDestroy { // TODO 
     this.userService.getProducerFromId(this.couponPass.owner).subscribe(user => {
       this.producer = user;
       this.couponService.setUserCoupon(this.producer);
-
     });
   }
-
 
 }
