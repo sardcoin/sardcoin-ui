2cfe387dfb5046905271a93ff4042a5aa89eb5eb
Andrea Catania <andreacatania21@gmail.com>
2019-07-25 09:55:19 +0200

    Fixed redeem

--- src/app/features/reserved-area/consumer/coupon-order/coupon-order.component.ts
+++ src/app/features/reserved-area/consumer/coupon-order/coupon-order.component.ts
@@ -15,6 +15,7 @@ import { environment } from '../../../../../environments/environment';
 import { UserService } from '../../../../shared/_services/user.service';
 import { ITEM_TYPE } from '../../../../shared/_models/CartItem';
 import { CouponToken } from '../../../../shared/_models/CouponToken';
+import {token} from 'flatpickr/dist/utils/formatting';
 
 @Component({
   selector: 'app-feature-reserved-area-consumer-order',
@@ -52,6 +53,7 @@ export class FeatureReservedAreaConsumerOrderComponent implements OnInit, OnDest
     let orderDetail: Order;
     let couponAux: Coupon;
     let coupons;
+    let verifier;
     let token, type;
     try {
       this.orders = await this.orderService.getOrdersByConsumer().toPromise();
@@ -68,13 +70,20 @@ export class FeatureReservedAreaConsumerOrderComponent implements OnInit, OnDest
         // Raggruppo i token per coupon_id
         coupons = _.groupBy(orderDetail.OrderCoupon, 'coupon_id');
 
+
         for(const coupon_id of Object.keys(coupons)) {
           token = coupons[coupon_id][0].coupon_token || coupons[coupon_id][0].package_token;
+          verifier = coupons[coupon_id][0].verifier;
           type = coupons[coupon_id][0].coupon_token ? ITEM_TYPE.COUPON : ITEM_TYPE.PACKAGE;
 
           couponAux = await this.couponService.getCouponByToken(token, type).toPromise();
           couponAux.quantity = coupons[coupon_id].length;
-          couponAux.token = coupons[coupon_id][0].coupon_token ||  coupons[coupon_id][0].package_token;
+
+          for (const coup of coupons[coupon_id]){
+             if(coup.verifier == null) {
+                couponAux.token = coup.coupon_token || coup.package_token;
+            }
+          }
 
 
           order.total += coupons[coupon_id].length * coupons[coupon_id][0].price;
