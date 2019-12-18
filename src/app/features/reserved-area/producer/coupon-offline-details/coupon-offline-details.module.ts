fd7d71e15a15eb23a07018612c4f19359ffdaf66
lele79 <porcu.lele@tiscali.it>
2019-12-18 16:21:23 +0100

    [fix] fix merge master

--- src/app/features/reserved-area/producer/coupon-offline-details/coupon-offline-details.module.ts
+++ src/app/features/reserved-area/producer/coupon-offline-details/coupon-offline-details.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { MatPaginatorIntl, MatPaginatorModule, MatSortModule, MatTableModule } from '@angular/material';
 import { BsDropdownModule, ModalModule } from 'ngx-bootstrap';
 import { BsModalService } from 'ngx-bootstrap/modal';
-import { BreadcrumbActions } from '../../../../../core/breadcrumb/breadcrumb.actions';
-import { SharedModule } from '../../../../../shared/shared.module';
+import { BreadcrumbActions } from '../../../../core/breadcrumb/breadcrumb.actions';
+import { SharedModule } from '../../../../shared/shared.module';
 import { CouponOfflineDetailsComponent } from './coupon-offline-details.component';
 
 @NgModule({
